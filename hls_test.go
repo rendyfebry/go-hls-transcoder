c62d7ae78180936a16fe5814083a79b7908ab190
Rendy Febry <rendyfebry@gmail.com>
2020-05-08 23:35:51 +0700

    Add basic unit test

---
+++ hls_test.go
@@ -0,0 +1,21 @@
+package hls
+
+import (
+	"fmt"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestCmdExecuteFfmpeg(t *testing.T) {
+	base, _ := os.Getwd()
+
+	targetPath := path.Join(base, "assets", "hls")
+	srcPath := path.Join(base, "assets", "raw", "sample.mov")
+	ffmpegPath := "/usr/local/bin/ffmpeg"
+
+	fmt.Println("targetPath", targetPath)
+	fmt.Println("srcPath", srcPath)
+
+	GenerateHLS(ffmpegPath, srcPath, targetPath, "1080p")
+}
