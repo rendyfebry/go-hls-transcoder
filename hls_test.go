8619c57eae9cee12c87d8607cbc742b918b91099
Rendy Febry <rendyfebry@gmail.com>
2020-05-09 20:16:36 +0700

    Fix go mod version

--- hls_test.go
+++ hls_test.go
@@ -3,13 +3,11 @@ package hls
 import (
 	"os"
 	"path"
-	"strings"
 	"testing"
 )
 
 func TestCmdExecuteFfmpeg(t *testing.T) {
 	base, _ := os.Getwd()
-	base = strings.Replace(base, "/hls", "", 1)
 
 	targetPath := path.Join(base, "assets", "hls")
 	srcPath := path.Join(base, "assets", "raw", "sample.mov")
